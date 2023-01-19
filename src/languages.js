9f5ad8042c93e8b9140a99be6b17517825f1b5a6
Lisbeth Quintero <lquintero28@live.com>
2023-01-19 16:54:10 -0500

    Removed unnecessary comments, daptation of skeleton screen for the favorites section, Improved styles in movie detail section

--- src/languages.js
+++ src/languages.js
@@ -1,32 +1,32 @@
 const languages = [
     {
-    "iso_639_1": "en",
-    "english_name": "English",
-    "name": "English"
+    'iso_639_1': 'en',
+    'english_name': 'English',
+    'name': 'English'
     },
     {
-    "iso_639_1": "fr",
-    "english_name": "French",
-    "name": "français"
+    'iso_639_1': 'fr',
+    'english_name': 'French',
+    'name': 'Français'
     },
     {
-    "iso_639_1": "de",
-    "english_name": "German",
-    "name": "Deutsche"
+    'iso_639_1': 'de',
+    'english_name': 'German',
+    'name': 'Deutsche'
     },
     {
-    "iso_639_1": "it",
-    "english_name": "Italian",
-    "name": "Italiano"
+    'iso_639_1': 'it',
+    'english_name': 'Italian',
+    'name': 'Italiano'
     },
     {
-    "iso_639_1": "zh",
-    "english_name": "Chinese",
-    "name": "漢語"
+    'iso_639_1': 'zh',
+    'english_name': 'Chinese',
+    'name': '漢語'
     },
     {
-    "iso_639_1": "es",
-    "english_name": "Spanish",
-    "name": "Español"
+    'iso_639_1': 'es',
+    'english_name': 'Spanish',
+    'name': 'Español'
     },
 ]
